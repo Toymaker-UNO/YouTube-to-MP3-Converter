37cd0845d01b080f5a9ea072f0510bc353521173
Toymaker <toycraftworld@gmail.com>
2025-05-01 20:36:08 +0900

    Update Download Button

--- controller/gui/ComboBox_AudioQuality.py
+++ controller/gui/ComboBox_AudioQuality.py
@@ -15,8 +15,7 @@ class ComboBox_AudioQuality:
     
     def __init__(self):
         self._window = None
-        self._url_input = None
-        self._current_url = ""
+        self._audio_quality = None
 
     def setup(self, window: QMainWindow):
         """ComboBox_AudioQuality를 초기화합니다.
@@ -29,13 +28,17 @@ class ComboBox_AudioQuality:
             self._audio_quality = self._window.findChild(QComboBox, "audio_quality")
             self.disable()
             if not self._audio_quality:
-                log.critical ("ComboBox_AudioQuality 초기화 실패")
+                log.critical("ComboBox_AudioQuality 초기화 실패")
 
     def enable(self):
         self._audio_quality.setEnabled(True)
     
     def disable(self):
         self._audio_quality.setEnabled(False)
+        
+    def get_selected_quality(self) -> str:
+        """선택된 오디오 품질을 반환합니다."""
+        return self._audio_quality.currentText()
 
 # 싱글톤 인스턴스 생성
 combo_box_audio_quality_instance = ComboBox_AudioQuality()
